3b26c78a1663700a5a3d16b585e5a6993f3fe725
samuel <samuel.baker@rack-a-tiers.com>
2024-06-22 20:40:41 -0700

    [ADD] added create video with text overlay function [UPD] updated readme.md [ADD] added and installed python-dotenv for env variables

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ShortsAutobot
 ShortsAutobot uses python, django and multiple libraries to pull popular Reddit posts and create a youtube short using pre-recorded gameplay footage for the background.
+
+# Features
+- Connects to Reddit API to pull top posts
+- Connects to youtube API to upload edited/created shorts
+- Connects to instabot API for uploading reels
--- shortsautobot/video_app/utils.py
+++ shortsautobot/video_app/utils.py
@@ -1,17 +1,28 @@
+from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
+from googleapiclient.http import MediaFileUpload
+from googleapiclient.discovery import build
 from django.conf import settings
+from dotenv import load_dotenv
+from instabot import Bot
 from gtts import gTTS
 import os
 
+# load in env variables here
+load_dotenv()
+
+# initialize the environment variables here for usage in this file
+INSTAGRAM_USERNAME = os.getenv('INSTAGRAM_USERNAME')
+INSTAGRAM_PASSWORD = os.getenv('INSTAGRAM_PASSWORD')
+
+# Get hot posts from a subreddit (in this case, we are using 'all' to get posts from all subreddits)
 def get_hot_reddit_posts(subreddit='all', limit=5):
-    """
-    Get hot posts from a subreddit
-    """
     posts = []
     for submission in settings.reddit.subreddit(subreddit).hot(limit=limit):
         posts.append({
             'title': submission.title,
             'url': submission.url,
             'seftext': submission.selftext,
+            ## Test variables below for future Usage
             # 'score': submission.score,
             # 'num_comments': submission.num_comments,
             # 'created_at': submission.created_utc,
@@ -20,9 +31,21 @@ def get_hot_reddit_posts(subreddit='all', limit=5):
         })
     return posts
 
+# Convert text to speech
 def text_to_speech(text, output_file):
-    """
-    Convert text to speech
-    """
     tts = gTTS(text=text, lang='en')
     tts.save(output_file)
+
+# Create a video with text overlay
+def create_video_with_text_overlay(video_path, text, output_path):
+    video = VideoFileClip(video_path)
+    txt_clip = TextClip(text, fontsize=24, color='white')
+    txt_clip = txt_clip.set_pos(('center', 'bottom')).set_duration(video.duration)
+    video = CompositeVideoClip([video, txt_clip])
+    video.write_videofile(output_path, codec='libx264')
+
+# Upload video to Instagram
+def upload_to_instagram(video_path, caption):
+    bot = Bot()
+    bot.login(username=INSTAGRAM_USERNAME, password=INSTAGRAM_PASSWORD)
+    bot.upload_video(video_path, caption=caption)
